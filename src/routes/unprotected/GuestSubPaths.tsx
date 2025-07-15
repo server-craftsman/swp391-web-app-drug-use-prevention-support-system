d4f0bc6657c517f94b312cdb1a83c97dfed4af87
HuuFuoc <tranhuuphuoccp@gmail.com>
2025-07-15 17:51:47 +0700

    hotfix

--- src/routes/unprotected/GuestSubPaths.tsx
+++ src/routes/unprotected/GuestSubPaths.tsx
@@ -17,7 +17,6 @@ import PaymentPageMain from "../../pages/customer/payment";
 import PaymentSuccess from "../../pages/customer/success";
 import PaymentFail from "../../pages/customer/cancel";
 import NotFoundPage from "../../pages/auth/not_found";
-import LessonDetailPage from "../../pages/customer/lesson";
 
 //================= PUBLIC SUB PATHS =================
 const UnauthorizedPage = lazy(() => import("../../pages/auth/unauthorized"));
@@ -32,7 +31,9 @@ const CartPage = lazy(() => import("../../pages/client/cart"));
 const ResetPasswordPage = lazy(() => import("../../pages/auth/reset_password"));
 const ConfirmEmailPage = lazy(() => import("../../pages/auth/confirm_email"));
 const AppointmentPage = lazy(() => import("../../pages/client/appointment"));
-const AssessmentResult = lazy(() => import("../../pages/client/assessment/result"));
+const AssessmentResult = lazy(
+  () => import("../../pages/client/assessment/result")
+);
 //======================================================
 
 // Export public sub paths - Only truly public routes here
